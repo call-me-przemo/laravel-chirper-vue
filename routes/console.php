609d1ec37af74b443ea02193bf61e0d65aca2964
Przemysław Mielniczek <pmiel13@gmail.com>
2025-02-18 21:49:15 +0100

    Creating Chirps

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('return-zero', fn() => 0)->purpose('Return 0');
+
+Artisan::command('return-one', fn() => 1)->purpose('Return 1');
